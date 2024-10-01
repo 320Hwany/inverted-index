0379bb26d79a5a3803e31baac995e5142eba0a36
Jung-YouHwan <yhwjd99@gmail.com>
2024-10-01 14:22:34 +0900

    feat : 초기 설정

---
+++ src/main/java/inverted_index/InvertedIndexApplication.java
@@ -0,0 +1,13 @@
+package inverted_index;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvertedIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvertedIndexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inverted_index/InvertedIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package inverted_index;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvertedIndexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
